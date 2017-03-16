0d268ef19948d092448a607ead1a4cb329cdda19
Marie Filbey <patriciamariefilbey@gmail.com>
2017-03-15 23:09:07 -0400

    Wednesday night commit.

--- src/js/config.js
+++ src/js/config.js
@@ -1,6 +1,6 @@
 function Config ($stateProvider, $urlRouterProvider) {
-  console.log($stateProvider);
-  console.log($urlRouterProvider);
+  //console.log($stateProvider);
+  //console.log($urlRouterProvider);
 
   $stateProvider
      .state('home', {
@@ -12,6 +12,16 @@ function Config ($stateProvider, $urlRouterProvider) {
        url: '/not-found',
        template: `<h2>So sorry! Unable to find that URL.</h2>`
      })
+     .state('add-contact', {
+       url: '/contacts',
+       templateUrl: 'templates/add-contact.tpl.html',
+       controller: 'ContactController'
+     })
+    //  .state('list-contact', {
+    //    url: '/contacts/:id',
+    //    template: 'templates/list-contact.tpl.html',
+    //    controller: 'ContactController'
+    //  })
 
   $urlRouterProvider.when('', '/home');
   $urlRouterProvider.otherwise('/not-found');
--- src/js/controllers/contact.js
+++ src/js/controllers/contact.js
@@ -4,11 +4,13 @@ const SERVER_URL = "https://shrouded-oasis-34673.herokuapp.com";
 function ContactController ($scope, $http) {
   console.log("Welcome to Contact controller");
   $scope.contacts = [];
+  // $scope.currentContact = ;
+  // console.log($scope.currentContact)
 
   function init () {
     $http.get(`${SERVER_URL}/contacts`).then(resp => {
       $scope.contacts = resp.data;
-      console.log($scope.contacts);
+      //console.log($scope.contacts);
     });
   }
   init();
@@ -17,7 +19,7 @@ function ContactController ($scope, $http) {
     var url = `${SERVER_URL}/contacts`;
     $http.post(url, data).then(resp => {
       $scope.contacts.push(resp.data);
-      console.log($scope.contacts);
+      console.log($scope.contacts.photo_URL);
     });
   };
 
